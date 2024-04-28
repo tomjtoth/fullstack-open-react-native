875be1e78399e96fc0f8783baa5250cd55840a7f
Tamás Tóth <tomjtoth@gmail.com>
2024-04-28 15:16:34 +0300

    10.8: `console.log('user:pass')` upon signIn

--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -1,7 +1,36 @@
+import { TextInput, Pressable, View } from 'react-native';
+import { useFormik } from 'formik';
+
 import Text from './Text';
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: (values) => {
+      const { username, password } = values;
+
+      console.log(`"${username}:${password}" logged in`)
+    },
+  });
+
+  return <View>
+    <TextInput
+      placeholder="username"
+      value={formik.values.username}
+      onChangeText={formik.handleChange('username')}
+    />
+    <TextInput
+      placeholder="password"
+      value={formik.values.password}
+      onChangeText={formik.handleChange('password')}
+    />
+    <Pressable onPress={formik.handleSubmit}>
+      <Text>Login</Text>
+    </Pressable>
+  </View >
 };
 
 export default SignIn;
